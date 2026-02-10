c8fd1cd6a89c9cfc8b1740701f32e072935b8fe8
Zorbas05 <Jonaslambrecht@web.de>
2026-02-10 12:00:11 +0100

    release: v1.5.9

--- scripts/build-release.cjs
+++ scripts/build-release.cjs
@@ -6,7 +6,7 @@ const banner = `// ==UserScript==
 // @name         GeoAnalyzr
 // @namespace    geoanalyzr
 // @author       JonasLmbt
-// @version      1.5.8
+// @version      1.5.9
 // @updateURL    https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.user.js
 // @downloadURL  https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.user.js
 // @match        https://www.geoguessr.com/*
