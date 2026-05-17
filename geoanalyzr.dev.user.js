8441702284a27affa48574f9c6722ac0ab95cbc1
JonasLmbt <Jonaslambrecht@web.de>
2026-05-17 12:27:13 +0200

    revert: restore original script names to avoid Tampermonkey duplicate installs (v2.6.7)

--- geoanalyzr.dev.user.js
+++ geoanalyzr.dev.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         GeoAnalyzr Dev
+// @name         GeoAnalyzr (Dev)
 // @namespace    geoanalyzr-dev
 // @author       JonasLmbt
-// @version      2.6.7-dev
+// @version      2.6.8-dev
 // @updateURL    https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.dev.user.js
 // @downloadURL  https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.dev.user.js
 // @icon         https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/images/logo-light.svg
--- geoanalyzr.sync.user.js
+++ geoanalyzr.sync.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         GeoAnalyzr Sync
+// @name         GeoAnalyzr (Minimal)
 // @namespace    geoanalyzr-sync
 // @author       JonasLmbt
-// @version      2.6.6
+// @version      2.6.7
 // @updateURL    https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.sync.user.js
 // @downloadURL  https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.sync.user.js
 // @icon         https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/images/logo-light.svg
