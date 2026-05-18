6692d13aa97e06f80e795f9ec7f62f4b31cd7ae1
JonasLmbt <Jonaslambrecht@web.de>
2026-05-18 10:39:51 +0200

    chore: bump version to 2.8.0

--- geoanalyzr.user.js
+++ geoanalyzr.user.js
@@ -2,7 +2,7 @@
 // @name         GeoAnalyzr
 // @namespace    geoanalyzr
 // @author       JonasLmbt
-// @version      2.7.2
+// @version      2.8.0
 // @updateURL    https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.user.js
 // @downloadURL  https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/geoanalyzr.user.js
 // @icon         https://raw.githubusercontent.com/JonasLmbt/GeoAnalyzr/master/images/logo-light.svg
